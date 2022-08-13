28f8bdc8c8715f64fc4043ca652f4e6d6bad83d9
steve02081504 <steve02081504@EDEN.fukwold>
2022-08-13 15:28:02 +0800

    v1.0.0.0

---
+++ builds/auto_upx.bat
@@ -0,0 +1,4 @@
+cd Release
+upx --ultra-brute --no-align --best --compress-resources=1 ghost_installer.exe
+rename ghost_installer.exe Taromati2_Installer.exe
+cd ..
--- src/ghost_installer.cpp
+++ src/ghost_installer.cpp
@@ -1,20 +1,36 @@
-#include <windows.h>
+﻿#include <windows.h>
 #include <shlobj_core.h>
-#include <shlwapi.h>
 
 #include "my-gists/ukagaka/SSP_Runner.hpp"
-#include "my-gists/windows/LoadStringFromResource.hpp"
+#include "my-gists/windows/WaitForXObjectWithMessageLoop.hpp"
+#include "my-gists/windows/get_temp_path.hpp"
+#include "my-gists/windows/download_file.hpp"
+#include "my-gists/windows/IsNetworkHasCost.hpp"
 
 #include "resource/resource.h"
 #include "ghost_installer.hpp"
 
-//shlwapi.lib
-#pragma comment(lib, "shlwapi.lib")
+namespace download_speed_up_thread {
+	void download_speed_up_ssp() {
+		try {
+			download_file_to_temp_dir(L"https://github.com/Taromati2/package-factory/releases/latest/download/ssp.exe", L"SSP.exe");
+		}
+		catch(const std::exception& e) {
+			MessageBoxA(NULL, e.what(), "Error", MB_OK);
+			throw;
+		}
+	}
+	void download_speed_up_nar() {
+		try {
+			download_file_to_temp_dir(L"https://github.com/Taromati2/package-factory/releases/latest/download/Taromati2.nar", L"Taromati2.nar");
+		}
+		catch(const std::exception& e) {
+			MessageBoxA(NULL, e.what(), "Error", MB_OK);
+			throw;
+		}
+	}
+}		// namespace download_speed_up_thread
 
-std::wstring download_temp_file(const std::wstring& url, const std::wstring& file_suffix);
-std::wstring get_ghost_url() {
-	return LoadStringFromResource(IDR_GHOST_URL);
-}
 
 namespace ssp_install {
 	std::wstring program_dir;
@@ -29,7 +45,7 @@ bool get_edit_Dia_text_as_path(std::wstring& path, HWND hDlg, WORD IDC) {
 										(LPARAM)0);
 	if(pathlen == 0) {
 		MessageBox(hDlg,
-				   L"No characters entered.",
+				   L"请输入路径",
 				   L"Error",
 				   MB_OK);
 		return false;
@@ -76,7 +92,7 @@ LRESULT CALLBACK InstallPathSelDlgProc(HWND hDlg, UINT message, WPARAM wParam, L
 				ZeroMemory(&bi, sizeof(bi));
 				bi.hwndOwner	  = hDlg;
 				bi.pszDisplayName = new wchar_t[MAX_PATH];
-				bi.lpszTitle	  = L"Select installation path";
+				bi.lpszTitle	  = L"选择SSP安装路径";
 				bi.ulFlags		  = BIF_RETURNONLYFSDIRS;
 				LPITEMIDLIST pidl = SHBrowseForFolder(&bi);
 				if(pidl != NULL) {
@@ -103,86 +119,123 @@ int APIENTRY WinMain(
 	_In_opt_ HINSTANCE hPrevInstance,
 	_In_ LPSTR		   lpCmdLine,
 	_In_ int		   nShowCmd) {
-	try {
-		SSP_Runner SSP;
-		if(SSP.IsInstalled()) {
-		install_ghost:
-			auto nar_file = download_temp_file(get_ghost_url(), L".nar");
-			SSP.install_nar(nar_file);
-			//We can't wait for ssp to terminate before deleting the nar file, because when ghost ends is up to the user
-			//So, no clearing of temporary files
+	//*
+	if(IsNetworkHasCost()) {
+		auto result = MessageBox(NULL,
+								 L"您的网络可能是计费网络，是否继续安装？",
+								 L"注意",
+								 MB_YESNO);
+		if(result == IDNO)
+			return 0;
+	}
+	//*/
+
+	SSP_Runner SSP;
+	HANDLE	   nar_download_thread = NULL;
+	auto	   downloading_ui	   = CreateDialogW(hInstance, (LPCTSTR)IDD_DOWNLOADING, NULL, NULL);
+	if(SSP.IsInstalled()) {
+		nar_download_thread = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)download_speed_up_thread::download_speed_up_nar, NULL, 0, NULL);
+	install_ghost:
+		if(!nar_download_thread) {
+			MessageBox(NULL,
+					   L"创建下载进程失败",
+					   L"Error",
+					   MB_OK);
+			return 1;
 		}
-		else {
+		ShowWindow(downloading_ui, SW_SHOW);
+		DWORD wait_result = WaitForSingleObjectWithMessageLoop(nar_download_thread, INFINITE);
+		ShowWindow(downloading_ui, SW_HIDE);
+		if(wait_result == WAIT_FAILED) {
+			MessageBox(NULL, L"下载nar失败", L"Error", MB_OK);
+			exit(1);
+		}
+		auto nar_file = std::wstring(get_temp_path()) + L"Taromati2.nar";
+		#ifndef _DEBUG
+			SSP.install_nar(nar_file, L"/o", L"callghost,deletesource");
+		#else
+			SSP.install_nar(nar_file, L"/o", L"callghost");
+		#endif
+		//We can't wait for ssp to terminate before deleting the nar file, because when ghost ends is up to the user
+		//So, no clearing of temporary files
+	}
+	else {
+		auto ssp_download_thread = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)download_speed_up_thread::download_speed_up_ssp, NULL, 0, NULL);
+		if(!ssp_download_thread) {
+			MessageBox(NULL,
+					   L"创建下载进程失败",
+					   L"Error",
+					   MB_OK);
+			return 1;
+		}
+		auto response = MessageBox(NULL,
+								   L"SSP未安装，此程序是运行ghost的基础平台\n点击确认以安装SSP并继续\n若已安装SSP，请关掉SSP并在接下来的安装路径选择中选择现有的SSP路径以更新SSP至最新版本",
+								   L"SSP未安装",
+								   MB_YESNO);
+		if(response != IDYES)
+			return 0;
+		{
+			//wait for ssp to download
+			DWORD wait_result = WaitForSingleObjectWithMessageLoop(ssp_download_thread, 0);
+			if(wait_result == WAIT_OBJECT_0) {
+				//start nar download
+				nar_download_thread = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)download_speed_up_thread::download_speed_up_nar, NULL, 0, NULL);
+			}
+			auto	   ssp_file = std::wstring(get_temp_path()) + L"SSP.exe";
+			EXE_Runner SSP_EXE(ssp_file);
+			//show install path dialog
+			ssp_install::program_dir = DefaultSSPinstallPath();
+			auto install_path_scl_ui = CreateDialogW(hInstance, (LPCTSTR)IDD_INSTALLATION_PATH_SELECTION, NULL, (DLGPROC)InstallPathSelDlgProc);
+			ShowWindow(install_path_scl_ui, SW_SHOW);
 			{
-				//download and install SSP
-				auto	   ssp_file = download_temp_file(L"http://ssp.shillest.net/archive/redir.cgi?stable&full", L".exe");
-				EXE_Runner SSP_EXE(ssp_file);
-				//show install path dialog
-				ssp_install::program_dir = DefaultSSPinstallPath();
-				auto install_path_scl_ui = CreateDialogW(hInstance, (LPCTSTR)IDD_INSTALLATION_PATH_SELECTION, NULL, (DLGPROC)InstallPathSelDlgProc);
-				ShowWindow(install_path_scl_ui, SW_SHOW);
-				{
-					MSG msg;
-					while(GetMessage(&msg, NULL, 0, 0)) {
-						TranslateMessage(&msg);
-						DispatchMessage(&msg);
-						if(ssp_install::ok_to_install)
-							break;
-					}
+				MSG msg;
+				while(GetMessage(&msg, NULL, 0, 0)) {
+					TranslateMessage(&msg);
+					DispatchMessage(&msg);
+					if(ssp_install::ok_to_install)
+						break;
 				}
-				DestroyWindow(install_path_scl_ui);
-				//create installation directory
-				switch(SHCreateDirectoryExW(NULL, ssp_install::program_dir.c_str(), NULL)) {
-				case ERROR_ALREADY_EXISTS:
-				case ERROR_SUCCESS:
-				case ERROR_FILE_EXISTS:
-					//install SSP
-					SSP_EXE.RunAndWait(L"-o\"" + ssp_install::program_dir + L"\"", L"-y");
-					//Delete temporary files
-					DeleteFileW(ssp_file.c_str());
-					break;
-				default:
-					//Delete temporary files
-					DeleteFileW(ssp_file.c_str());
-					MessageBoxW(NULL, L"Could not create installation directory.", L"Error", MB_OK);
+			}
+			DestroyWindow(install_path_scl_ui);
+			//create installation directory
+			switch(SHCreateDirectoryExW(NULL, ssp_install::program_dir.c_str(), NULL)) {
+			case ERROR_ALREADY_EXISTS:
+			case ERROR_SUCCESS:
+			case ERROR_FILE_EXISTS: {
+				//wait for ssp to download
+				ShowWindow(downloading_ui, SW_SHOW);
+				DWORD wait_result = WaitForSingleObjectWithMessageLoop(ssp_download_thread, INFINITE);
+				ShowWindow(downloading_ui, SW_HIDE);
+				if(wait_result != WAIT_OBJECT_0) {
+					MessageBox(NULL, L"SSP下载失败", L"SSP下载失败", MB_OK);
 					return 1;
 				}
+				//close the ssp download thread
+				CloseHandle(ssp_download_thread);
+				//start nar download
+				if(!nar_download_thread)
+					nar_download_thread = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)download_speed_up_thread::download_speed_up_nar, NULL, 0, NULL);
+				//install SSP
+				SSP_EXE.RunAndWait(L"-o\"" + ssp_install::program_dir + L"\"", L"-y");
+				break;
+			}
+			default:
+				MessageBoxW(NULL, L"未能创建安装文件夹\n请考虑以管理员运行此程序或检查安装路径", L"Error", MB_OK);
+				return 1;
 			}
 			//set SSP_Runner's path
 			SSP.reset_path(ssp_install::program_dir + L"\\ssp.exe");
-			if(!SSP.IsInstalled())
-				MessageBoxW(NULL, L"Could not install SSP.", L"Error", MB_OK);
-			//delete ghost dir because the Japanese based Emily will plague users of other languages
-			//using SHFileOperation
-			//Does not delete the balloon folder as ghost may not come with a balloon
-			{
-				auto			ghost_dir = ssp_install::program_dir + L"\\ghost";
-				SHFILEOPSTRUCTW op;
-				ZeroMemory(&op, sizeof(op));
-				op.wFunc  = FO_DELETE;
-				op.pFrom  = ghost_dir.c_str();
-				op.fFlags = FOF_SILENT | FOF_NOCONFIRMATION | FOF_NOERRORUI | FOF_NOCONFIRMMKDIR;
-				SHFileOperationW(&op);
+			if(!SSP.IsInstalled()) {
+				MessageBoxW(NULL, L"未能安装SSP", L"Error", MB_OK);
+				return 1;
 			}
-			//get language id
-			auto langid = GetUserDefaultUILanguage();
-			//install language pack
-			auto langpack_url = L"http://ssp.shillest.net/archive/redir.cgi?stable&langpack=" + std::to_wstring(langid);
-			try {
-				auto langpack_file = download_temp_file(langpack_url, L".nar");
-				SSP.install_nar(langpack_file);
-			}
-			catch(...) {
-			}
-			//install ghost
-			goto install_ghost;
+			//Delete temporary files
+			DeleteFileW(ssp_file.c_str());
 		}
-		return 0;
+		//install ghost
+		goto install_ghost;
 	}
-	catch(const std::exception& e) {
-		MessageBoxA(NULL, e.what(), "Error", MB_OK);
-		return 1;
-	}
+	return 0;
 }
 
 std::wstring DefaultSSPinstallPath() {
